375ecfbc2d35d1f1b6421b57554775cf78000132
eunyeo1 <tjejqn234@naver.com>
2023-03-16 10:25:45 +0900

    2-3 특수문

--- src/main.c
+++ src/main.c
@@ -1,3 +1,18 @@
+
+// exercise 2-3
+# include <stdio.h>
+int main(void)
+
+{  
+    printf("\t Hello C! \n Hello world! \n");
+    printf("큰따옴표 : \" \" \n");
+    printf("작은따옴표 : \' \' \n");
+    printf("역슬래시 : \\ \n");
+
+    return 0;
+}
+
+/*
 // exercise 2-2
 #include <stdio.h>
 
@@ -6,8 +21,6 @@ int main(void)
     printf("heollo C world \n!");
     return 0;
 }
-
-/*
 //exercise2-1
 #include <stdio.h>
 
