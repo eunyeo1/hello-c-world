67de70cdd0ac0c102626b632e02466db37764c55
eunyeo1 <tjejqn234@naver.com>
2023-03-16 17:31:51 +0900

    2-12

--- src/main.c
+++ src/main.c
@@ -1,4 +1,26 @@
 
+ //exercise 2-12
+#include <stdio.h>
+int main(void)
+{
+    int a,b,c;
+
+    printf("10진수 정수 1개 입력 : ");
+    scanf("&d",&a);
+    printf("10진수 : %d,16진수 : %x,8진수 : %o \n",a,a,a);
+
+    printf("16진수 정수 1개 입력 : ");
+    scanf("&x",&b);
+    printf("10진수 : %d,16진수 : %x,8진수 : %o \n",b,b,b);
+
+    printf("8진수 정수 1개 입력 : ");
+    scanf("&o",&c);
+    printf("10진수 : %d,16진수 : %x,8진수 : %o \n",c,c,c);
+
+    return 0;
+
+}
+/*
  
  //exercise 2-11
  #include <stdio.h>
@@ -12,7 +34,6 @@
     return 0; 
  }
 
-/*
 //exercisen2-10
 #include <stdio.h>
 int main(void)
