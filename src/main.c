7c3150b540236ffc9bc70e5f9a8046a88fc1f097
eunyeo1 <tjejqn234@naver.com>
2023-03-17 09:21:37 +0900

    3-9

--- src/main.c
+++ src/main.c
@@ -1,4 +1,21 @@
 
+//exercise 3-9
+#include <stdio.h>
+int main(void)
+{
+    int a=3;
+    int b=4;
+
+    printf("a의 값 : %d \n",a);
+    printf("b의 값 : %d \n",b);
+
+    printf("변수 a의 시작 주소 : %x \n", &a);
+    printf("변수 b의 시작 주소 : %x \n", &b);
+
+    return 0;
+}
+
+/*
 //exercise 3-8
 #include <stdio.h>
 int main(void)
@@ -15,7 +32,6 @@ int main(void)
 }  
 
 
-/* 
 //exercise 3-7
 #include <stdio.h>
 int main(void)
