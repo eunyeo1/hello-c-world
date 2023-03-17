67cb2b6a2685c3873e7e81cb3147e23a2f0f5117
eunyeo1 <tjejqn234@naver.com>
2023-03-17 09:51:57 +0900

    4-5

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
+ 
+//exercise 4-5
+#include <stdio.h>
+int main(void)
+{
+    printf("문자열 상수는 %s입니다. \n","A");
+    printf("문자열 상수는 %s입니다. \n","10+10");
+    printf("문자열 상수는 %s입니다.", "Hi, everyone");
 
-
+    return 0;   
+    
+}
+/*
 //exercise 4-4
 #include <stdio.h>
 int main(void)
@@ -19,7 +30,6 @@ int main(void)
 
 }
 
-/*
 //exercise 4-3
 #include <stdio.h>
 int main(void)
