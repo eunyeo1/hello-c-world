74ee5e1df523795038766a2a4b2235279dfe4fe8
eunyeo1 <tjejqn234@naver.com>
2023-04-14 17:54:17 +0900

    part1_5-15-2
    - test.c와 test.h로 파일을 나눔
    - 물음표는 해결해야 함

---
+++ src/helloworldcpp/test.h
@@ -0,0 +1,54 @@
+void ex5_15();
+void ex5_14();
+void ex5_13();
+void ex5_12();
+void ex5_11();
+
+void ex5_10();
+char* string1(void);
+char* string2(void);
+
+void ex5_9();
+int* func();
+
+void ex5_8();
+void ex5_7();
+void ex5_6();
+void ex5_5();
+void ex5_4();
+void ex5_3();
+void ex5_2();
+void ex5_1();
+
+void ex4_8();
+void ex4_7();
+void ex4_6();
+void ex4_5();
+void ex4_4();
+void ex4_3();
+void ex4_2();
+void ex4_1();
+
+void ex3_9();
+void ex3_8();
+void ex3_7();
+void ex3_6();
+void ex3_5();
+void ex3_4();
+void ex3_3();
+void ex3_2();
+void ex3_1();
+
+void ex2_13();
+void ex2_12();
+void ex2_11();
+void ex2_10();
+void ex2_9();
+void ex2_8();
+void ex2_7();
+void ex2_6();
+void ex2_5();
+void ex2_4();
+void ex2_3();
+void ex2_2();
+void ex2_1();
