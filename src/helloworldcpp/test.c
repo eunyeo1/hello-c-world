41788cd2b7c75216142fcbc357c9662841cecf69
eunyeo1 <tjejqn234@naver.com>
2023-05-03 17:14:20 +0900

    7-13

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -9,6 +9,18 @@
 int num;
 
 
+
+void ex7_13()
+{
+    int i=1, factorial=1;
+    for(; i<=10; i++)
+    {
+        factorial=factorial+i;
+    }
+    printf("1부터 10까지의 곱 : %d \n",factorial);
+ 
+}
+
 void ex7_12()
 {
     int i;
