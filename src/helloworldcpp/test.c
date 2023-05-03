eb3bb4188ff8f7823decb575aa44bc3fd73b924d
eunyeo1 <tjejqn234@naver.com>
2023-05-03 16:35:52 +0900

    7-7

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,23 @@
 #define CODE 
 int num;
 
+
+
+void ex7_7()
+{
+    int i, sum=0;
+    for (i=0; i<=10; i++)
+    {
+        sum+sum+i;
+        printf("i=%d,sum=%d \n", i, sum);
+
+    }
+    printf("-------반복문 종료------\n");
+
+}
+
+
+
 void ex7_6()
 {
     for(num=0; num<5; num++)
