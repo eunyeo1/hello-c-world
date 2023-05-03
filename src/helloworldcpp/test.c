2ec3e052c8332a78b624c9bc248d4e6ea9888e54
eunyeo1 <tjejqn234@naver.com>
2023-05-03 14:45:00 +0900

    6-4

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,24 @@
 #define CODE 
 int num;
 
+void ex6_4()
+{
+    char num1=-129;
+    char num2=128;
+
+    printf("%d \n", num1);
+    printf("%d \n", num2);
+
+    num1=-130;
+    num2=129;
+
+    printf("%d \n",num1);
+    printf("%d \n", num2);
+
+
+}
+
+
 
 void ex6_3()
 {
