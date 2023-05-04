cdc388a9e8b4d12e2f8da3012499540a0183f26d
eunyeo1 <tjejqn234@naver.com>
2023-05-04 09:38:56 +0900

    8-7

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,26 @@
 #define CODE 
 int num;
 
+
+
+void ex8_7()
+{
+    printf("c언어 성적 입력 : ");
+    scanf("%d",&num);
+
+    if(num>=95)
+        printf("A+입니다. \n");
+    else if(num>=90)
+        printf("A입니다. \n");
+    else if(num>=85)
+        printf("B+입니다. \n");
+    else if(num>=80)
+        printf("B입니다. \n");
+    else
+        printf("F입니다.\n");
+ 
+}
+
 void ex8_6()
 {
     printf("숫자 입력: ");
