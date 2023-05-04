c43fd1e5a5c253c09dc38d830d20be76f052581e
eunyeo1 <tjejqn234@naver.com>
2023-05-04 10:26:27 +0900

    9-2
    9-1은 일부로 에러 만드는 예제여서 제외함

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,22 @@
 #define CODE 
 int num;
 
+
+
+void ex9_2()
+{
+    int i, j;
+    int k;
+    
+
+    printf("숫자 두개를 입력하세요 : ");
+    scanf("%d %d", &i, &j);
+
+    k = max(i, j);
+    printf("%d와 %d 중 큰 수는 %d입니다.\n", i, j, k);
+}
+
+
 void ex8_13()
 {
     int num;
