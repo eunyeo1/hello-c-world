f4e338f4490074ffcdc85f239d5155e14229cb82
eunyeo1 <tjejqn234@naver.com>
2023-05-08 17:48:05 +0900

    1-3

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,18 @@
 #define CODE 
 int num;
 
+void ex1__3()
+{
+    int array[3]={87,99,80};
+    int total=0;
+
+    total=array[0]+array[1]+array[2];
+    printf("총점은 %d이고", total);
+    printf("평균은 %.21f입니다. \n",(double)total/3);
+ 
+
+}
+
 void ex1__2()
 
 {
