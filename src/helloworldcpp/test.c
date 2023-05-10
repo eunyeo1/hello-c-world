d34e0fef311c331d837d44a0c1e54fee79d8575a
eunyeo1 <tjejqn234@naver.com>
2023-05-10 17:51:57 +0900

    2-5

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,26 @@
 #define CODE 
 int num;
 
+void ex2__5()
+{
+    int i,j,k;
+    int array[3][3][3]={{1,2,3,4,5,6,7,8,9},{10,11,12,13,14,15,16,17,18},{19,20,21,22,23,24,25,26,27}};
+    for(i=0;i<3;i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            for(k=0; k<3; k++)
+            {
+                printf("%d",array[i][j][k]);
+            }
+            printf("\n");
+        }
+            printf("-----------\n");
+    }
+
+ 
+}
+
 void ex2__4()
 {
     int array[2][2];
