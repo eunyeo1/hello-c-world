c343cbdb81cc27a10adb3fcd41995b12cf0ba1c5
eunyeo1 <tjejqn234@naver.com>
2023-05-11 15:04:57 +0900

    4-12

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,26 @@
 #define CODE 
 int num;
 
+void ex4__12()
+{
+    int array[3][3]={10,20,30,40,50,60,70,80,90};
+
+    printf("%x %x %x \n", &array[0][0],&array[0][1],&array[0][2]);
+    printf("%x %x %x \n", &array[1][0],&array[1][1],&array[1][2]);
+    printf("%x %x %x \n", &array[2][0],&array[2][1],&array[2][2]);
+    printf("-----------\n");
+
+    printf("%d %d %d \n", &array[0][0],&array[0][1],&array[0][2]);
+    printf("%d %d %d \n", &array[1][0],&array[1][1],&array[1][2]);
+    printf("%d %d %d \n", &array[2][0],&array[2][1],&array[2][2]);
+    printf("-----------\n");
+
+    printf("%d %d %d \n", &array[0][0],&array[0][1],&array[0][2]);
+    printf("%d %d %d \n", &array[1][0],&array[1][1],&array[1][2]);
+    printf("%d %d %d \n", &array[2][0],&array[2][1],&array[2][2]);
+}
+
+
 void ex4__11()
 {
     int array[3];
