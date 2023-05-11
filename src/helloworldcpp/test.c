340834f5488b59119a16af74d3c983aa1d8cb902
eunyeo1 <tjejqn234@naver.com>
2023-05-11 10:13:28 +0900

    2-12

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,24 @@
 #define CODE 
 int num;
 
+void ex2__12()
+{
+    int array[2][2]={10,20,30,40};
+
+    printf("%d %d \n",*&array[0][0],*&array[0][1]);
+    printf("%d %d \n",*&array[1][0],*&array[1][1]);
+    printf("----------------\n");
+
+    printf("%d %d \n",*(array[0]+0),*(array[0]+1));
+    printf("%d %d \n",*(array[1]+0),*(array[1]+1));
+    printf("----------------\n");
+
+    printf("%d %d \n",*(*(array+0)+0),*(*(array+0)+1));
+    printf("%d %d \n",*(*(array+1)+0),*(*(array+1)+1));
+ 
+}
+
+
 void ex2__11()
 {
     int array[2][2]={10,20,30,40};
