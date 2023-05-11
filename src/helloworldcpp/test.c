0cc78b42ad230899d32e9c5f5d86e1c8f0630178
eunyeo1 <tjejqn234@naver.com>
2023-05-11 10:17:56 +0900

    3-1
    [ 포인터란 무엇인가 ]

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,19 @@
 #define CODE 
 int num;
 
+void ex3__1()
+{
+    int num1=3;
+    char num2='A';
+
+    printf("주소 : %x, 값 : %d, 값: %d \n",&num1,num1,*&num1);
+    printf("주소 : %x, 값 : %d, 값: %d \n",&num2,num2,*&num2);
+
+    printf("%d %d \n",sizeof(int), sizeof(char));
+    printf("%d %d \n",sizeof(num1), sizeof(num2));
+
+} 
+
 void ex2__12()
 {
     int array[2][2]={10,20,30,40};
