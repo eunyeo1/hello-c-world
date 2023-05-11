4a555725bf940f378de29ae94e2128ffe148328e
eunyeo1 <tjejqn234@naver.com>
2023-05-11 13:27:46 +0900

    4-5

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,18 @@
 #define CODE 
 int num;
 
+void ex4__5()
+{
+    int array[3]={10,20,30};
+    int* p=NULL;
+    p=array;
+
+    printf("%d %d %d \n", *p,*(p+0),*&p[0]);
+    printf("%d %d \n",*(p+1),*&p[1]);
+    printf("%d %d \n",*(p+2),*&p[2]);
+
+}
+
 void ex4__4()
 {
     int array[3]={10,20,30};
