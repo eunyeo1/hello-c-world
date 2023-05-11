a5d6ca32e2c48b499bdefed7410603928c7b2f0c
eunyeo1 <tjejqn234@naver.com>
2023-05-11 14:41:15 +0900

    4-11

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,32 @@
 #define CODE 
 int num;
 
+void ex4__11()
+{
+    int array[3];
+    int* p=NULL;
+   
+    p=array;
+    *p=10;
+    printf("&d %d %d \n",p[0],p[1],p[2]);
+    printf("-----------\n");
+
+    p=p+1;
+    *p=20;
+    printf("&d %d %d \n",p[-1],p[0],p[1]);
+    printf("-----------\n");
+
+    p=p+1;
+    *p=30;
+    printf("&d %d %d \n",p[-2],p[-1],p[0]);
+    printf("-----------\n");
+
+    printf("&d %d %d \n",p[-2],p[-1],p[0]);   
+    printf("&d %d %d \n",*(p-2),*(p-1),*p);
+    printf("-----------\n");
+
+}
+
 void ex4__10()
 {
     int array[3]={10,20,30};
