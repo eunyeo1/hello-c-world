5dff534a4445218248b5b3c213a55977e2794c67
eunyeo1 <tjejqn234@naver.com>
2023-05-12 15:30:26 +0900

    4-21

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,41 @@
 #define CODE 
 int num;
 
+void ex4_21()
+{
+    int a=10, b=20, c=30;
+    int* ap[3]=(NULL,NULL,NULL);
+
+    int array[2][3]={10,20,30,40,50,60};
+    int (*p)[3]=NULL;
+
+    ap[0]=&a;
+    ap[1]=&b;
+    ap[2]=&c;
+
+    printf("%x %x %x \n",&a,&b,&c);
+    printf("%x %x %x \n", ap[0],ap[1],ap[2]);
+    printf("%x %x %x \n",*(ap+0),*(ap+1),*(ap+2));
+    printf("---------------\n");
+
+    printf("%d %d %d \n",&a,&b,&c);
+    printf("%d %d %d \n", ap[0],ap[1],ap[2]);
+    printf("%d %d %d \n",**(ap+0),**(ap+1),**(ap+2));
+    printf("---------------\n");
+    p=array;
+
+    printf("%d %d %d \n",*(p[0]+0),*(p[0]+1),*(p[0]+2));
+    printf("%d %d %d \n",*(p[1]+0),*(p[1]+1),*(p[1]+2));
+    printf("---------------\n");
+
+    printf("%d %d %d \n",*(*(p+0)+0),*(*(p+0)+1),*(*(p+0)+2));
+    printf("%d %d %d \n",*(*(p+1)+0),*(*(p+1)+1),*(*(p+1)+2));
+    printf("---------------\n");
+
+    return;
+}
+
+
 void ex4_20()
 {
     int a=10, b=20, c=30;
