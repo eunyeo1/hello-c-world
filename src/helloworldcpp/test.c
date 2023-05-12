f350af9089a759a70b73a3ebff41ab5518f7baba
eunyeo1 <tjejqn234@naver.com>
2023-05-12 15:21:54 +0900

    4-16

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,19 @@
 #define CODE 
 int num;
 
+void ex4__16()
+{
+    int array[2][3]={10,20,30,40,50,60};
+    int(*p)[3]=NULL;   
+
+    p=array;
+
+    printf("%d %d %d \n", p[0][0],p[0][1],p[0][2]);
+    printf("%d %d %d \n", p[1][0],p[1][1],p[1][2]);
+
+}
+
+
 void ex4__15()
 {
     int array1[2][3]={10,20,30,40,50,60};
