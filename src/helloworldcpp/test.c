3bf6bccf97e579250f991ceb2b0673206383413b
eunyeo1 <tjejqn234@naver.com>
2023-05-15 09:28:04 +0900

    4-32

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,31 @@
 #define CODE 
 int num;
 
+void ex4_32()
+{
+    char*p="good morning";
+    char*q="C-language";
+    char*array[2]={"good morning","C-language"};
+
+    printf("%s \n",p);
+    printf("%s \n",q);   
+    printf("---------\n");
+
+    printf("%s \n",array[0]);
+    printf("%s \n",array[1]);   
+    printf("---------\n");
+
+    printf("%s \n",p+5);
+    printf("%s \n",q+2);   
+    printf("---------\n");
+
+    printf("%s \n",array[0]+5);
+    printf("%s \n",array[1]+2);
+
+
+}
+
+
 void ex4_31()
 {
     char*p=&"ABCD";
