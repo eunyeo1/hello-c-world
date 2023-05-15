ab96cf398d03770c334f450e95cb2efa1610494a
eunyeo1 <tjejqn234@naver.com>
2023-05-15 15:05:10 +0900

    1-1
    part3 [구조체와 공용체란 무엇인가]

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,22 @@
 #define CODE 
 int num;
 
+void ex1___1()
+ 
+{
+    struct group g1;
+    
+    g1.a=10;
+    g1.b=1.1234;
+
+    printf("g1.a의 값 : %d \n",g1.a);
+    printf("g1.b의 값 : %if \n",g1.b);
+
+    return 0;
+}
+
+//////////////////////////// part 3 
+
 void ex5___16()
 {
     char c=3;
