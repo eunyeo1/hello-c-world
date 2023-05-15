d2c7037d0f15b63b06a6da54b3fe1eca967a173c
eunyeo1 <tjejqn234@naver.com>
2023-05-15 09:41:01 +0900

    5-4

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,26 @@
 #define CODE 
 int num;
 
+void ex5___4()
+{
+    int array[2][4]={10,20,30,40,50,60,70,80};
+    func(array,sizeof(array)/16,sizeof(array)/8);
+    return 0;
+}
+void func(int (*p)[4],int num1,int num2)
+{
+    int i,j;
+    for(i=0; i<num2; j++)
+    {
+        for(j=0; j<num2; j++)
+        {
+           printf("%d",p[i][j]);
+        }
+        printf("\n"); 
+    }
+}
+
+
 void ex5___3()
 {
     int array[]={10,20,30,40,50,60,70,80};
