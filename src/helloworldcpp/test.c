509edc84262da7f2c005385670f009ae12a2aa8f
eunyeo1 <tjejqn234@naver.com>
2023-05-15 09:45:01 +0900

    5-8

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,22 @@
 #define CODE 
 int num;
 
+void ex5___8()
+{
+    int* p=NULL;
+    p=func();
+
+    printf("%d %d %d %d \n",p[0],p[1],p[2],p[3]);
+    printf("%d %d %d %d",*(p+0),*(p+1),*(p+2),*(p+3));
+    return 0;
+}
+int* func()
+{
+    int array[]={10,20,30,40,};
+    return array;
+}
+
+
 void ex5___7()
 {
     int* p=NULL;
