54eedb6c0eb3fb08e2b5cc2801fe9fbd9812ff79
eunyeo1 <tjejqn234@naver.com>
2023-05-15 09:42:32 +0900

    5-6
    5-5은 에러 만드는 문제

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,23 @@
 #define CODE 
 int num;
 
+void ex5___6()
+{
+    int* p=NULL;
+
+    p=input();
+    printf("%d \n",*p);
+
+    return 0;
+} 
+int* input()
+{
+    int num1;
+    scanf("&d",&num1);
+    return &num1;
+}
+
+
 void ex5___4()
 {
     int array[2][4]={10,20,30,40,50,60,70,80};
