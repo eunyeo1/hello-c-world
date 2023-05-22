ca815326765be2943a473e5086839fdc11a50b04
eunyeo1 <tjejqn234@naver.com>
2023-05-22 16:11:24 +0900

    3-4

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,29 @@
 #define CODE 
 int num;
 
+void ex3___4()
+{
+    FULE* stream;
+    int file_state;
+    int input=0;
+
+    stream=fopen("datal.txt","w");
+    if(stream==NULL)
+        printf("파일 열기 에러");
+
+    puts("데이터 입력");
+    while(input !=EOF)
+    {
+        input=fgetc(stdin);
+        fputc(input,stream);
+    }
+    
+    file_state=fclose(stream);
+    if(file_state==EOF)
+        puts("파일 닫기 에러");
+}
+
+
 void ex3___3()
 {
     FULE* stream;
