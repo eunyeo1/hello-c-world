e279f1cad8adaa948e989f2311c9d914f1735fcd
eunyeo1 <tjejqn234@naver.com>
2023-05-22 13:57:20 +0900

    2-17

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,20 @@
 #define CODE 
 int num;
 
+void ex2___17()
+{
+    int i=0;
+    puts("10개의 난수를 발생시킵니다.");
+    srand(1);
+
+    while(i<10)
+    {
+        printf("%d \t",rand());
+        i=i+1;
+    }
+}
+
+
 void ex2___16()
 {
     int i=0;
