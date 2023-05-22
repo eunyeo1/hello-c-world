c8be46c93089c03d15f4f35c460e26fd9c5f295a
eunyeo1 <tjejqn234@naver.com>
2023-05-22 10:34:39 +0900

    2-13

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,7 +8,24 @@
 #define CODE 
 int num;
 
+void ex2___13()
+{
+    char* str1="3.14";
+    char* str2="100";
+    char* str3="10000000";
 
+    double num1;
+    int num2;
+    long num3;
+
+    num1=atof(str1);
+    num2=atoi(str2);
+    num3=atol(str3);
+
+    printf("%lf, %d, %ld \n",num1,num2,num3);
+
+
+}
 
 void ex2___12()
 {
