0181394474487ec5e95ddfcdd2786b2b6ad8ea25
eunyeo1 <tjejqn234@naver.com>
2023-05-23 14:38:30 +0900

    3-11

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,24 @@
 #define CODE 
 int num;
 
+
+void ex3___11()
+{
+    int buffer1[5]={0xff,0x56,0x78,0xfa,0xf1};
+    int buffer2[5];
+
+    FILE* stream;
+    stream=fopen("student.dat","wb");
+    fwrite(buffer1,sizeof(int), 5, stream);
+    fclose(stream);
+
+    stream=fopen("student.dat","rb");
+    fread(buffer2,sizeof(int),5,stream);
+    printf("%x %x %x %x %x \n", buffer2[0], buffer2[1], buffer2[2],buffer2[3],buffer2[4]);
+    fclose(scream);
+
+}
+
 void ex3___10()
 {
     int age;
