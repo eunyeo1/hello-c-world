6041752fe2374c337ea426e55d61afe4fdcb19e5
eunyeo1 <tjejqn234@naver.com>
2023-05-23 17:33:41 +0900

    4-2
    [동적 메모리 할당과 가변인자]

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,22 @@
 #define CODE 
 int num;
 
+void ex4___2()
+{
+    int* p=NULL;
+    p=(int*)malloc(4);
+
+    if(p==NULL)
+        printf("힙 영역에 동적 메모리 할당 실패 \n");
+    
+    *p=10;
+    printf("주소 : %x \n",p);
+    printf("값 : %d \n", *p);
+
+    free(p);
+    p=NULL;
+}
+
 
 void ex4___1()
 {
