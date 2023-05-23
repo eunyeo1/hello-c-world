9654a50c524bbd61ad32fce31e23f5831cbb5b54
eunyeo1 <tjejqn234@naver.com>
2023-05-23 13:11:53 +0900

    3-7

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,25 @@
 #define CODE 
 int num;
 
+void ex3___7()
+{
+    FILE* stream;
+    char name[10];
+    int kor, eng, total;
+
+    printf("1. 이름 입력 : ");
+    fscanf(stdin,"%s",name);
+
+    printf("2. 국어 점수, 영어 점수 입력 : ");
+    fscanf(stdin,"%d %d",&kor,&eng);
+    total=kor+eng;
+
+    stream=fopen("data4.txt","W");
+    fprintf(stream,"%s %d %d %d \n", name, kor, eng, total);
+    fclose(stream);
+
+}
+
 
 void ex3___6()
 {
