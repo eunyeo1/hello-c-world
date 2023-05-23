f9d2fbf905c6645f23b6d796104c7b2c2741f19c
eunyeo1 <tjejqn234@naver.com>
2023-05-23 13:37:13 +0900

    3-8

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,25 @@
 #define CODE 
 int num;
 
+void ex3___8()
+{
+    FILE* stream1;
+    FILE* stream2;
+
+    char name[10]=" ";
+    int kor=0, eng=0, total=0;
+
+    stream1=fopen("data4.txt","r");
+    stream2=fopen("data5.txt","w");
+
+    fscanf(stream1, "%s %d %d %d \n", name, &kor, &eng, &total);
+    fscanf(stream2, "%s %d %d %d \n", name, kor, eng, total);
+
+    fclose(stream1);
+    fclose(stream2);
+
+}
+
 void ex3___7()
 {
     FILE* stream;
