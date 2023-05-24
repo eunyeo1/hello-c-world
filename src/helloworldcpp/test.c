538217e148b132c521a0208f7cecf6fd80cd9974
eunyeo1 <tjejqn234@naver.com>
2023-05-24 11:25:03 +0900

    4-8

--- src/helloworldcpp/test.c
+++ src/helloworldcpp/test.c
@@ -8,6 +8,27 @@
 #define CODE 
 int num;
 
+void ex4___8()
+{
+    int a=10,b=20,c=30;
+
+    add(1,a);
+    add(2,a,b);
+    add(3,a,b,c);
+}
+{
+    int* p=NULL;
+    p=&num+1;
+
+    if(num==1)
+        printf("%d \n",p[0]);
+    else if(num==2)
+        printf("%d \n",p[0]+p[1]);
+    else 
+        printf("%d \n",p[0]+p[1]+p[3]);
+}
+
+
 void ex4___7()
 {
     int i=0;
