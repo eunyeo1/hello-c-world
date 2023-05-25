57929d8e81688b85d13162b4cadd9370e3c25bf9
eunyeo1 <tjejqn234@naver.com>
2023-05-25 11:30:03 +0900

    5-22

--- src/helloworldcpp/main.c
+++ src/helloworldcpp/main.c
@@ -2,9 +2,4 @@
 #include "test.h"
 
 int main() 
-{
-    printf("반지름 3의 원의 둘레 : %lf \n",circle(3));
-    printf("반지름 3의 원의 넓이 : %lf \n",area(3));
 
-    return 0;
-}
--- src/helloworldcpp/test.h
+++ src/helloworldcpp/test.h
@@ -1,3 +1,406 @@
+void ex5___22()
+    { 
+        printf("Hello C \n");
+    
+    return 0;
+}
+
+
+
+
+void ex5___21()
+extern double circle(int radius);
+extern double area(int radius);
+
+
+
+void ex5___20-2()
+extern double circle(int radius)
+extern double area(int radius)
+
+
+void ex5___20-1()
+double circle(int radius)
+
+double area(int radius)
+
+
+void ex5___20()
+double circle(int radius);
+double area(int radius);
+
+double circle(int radius)
+
+double area(int radius)
+
+
+void ex5___19-2()
+extern int num1,num2;
+extern void add(int num1,int num2);
+
+
+void ex5___19-1()
+static int num1=10;, num2=20;
+
+static void add(num1,num2);
+
+
+
+void ex5___18()
+int num1=10, num2=20;
+void add(num1,num2)
+
+
+void ex5___17()
+int a=6,b=3;
+
+void ex5___16()
+#ifndef ADD
+    #define ADD
+#endif
+
+#ifndef MUL
+    #define MUL
+#endif
+
+
+
+
+void ex5___6()
+int mul(x,y);
+int mulx*y;
+
+void ex4___9()
+int* a(int num, ...);
+
+
+void ex4___8()
+void add(int num, ...);
+
+void ex4___1()
+int a=10;
+
+void ex3___12();
+struct score
+{
+    char name[20];
+    double kor,eng,total;
+};
+typedef struct score STUDENT;
+
+
+void ex1___29();
+enum week {ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN};
+enum season {SPRING,SUMMER=2,FALL,WINTER};
+
+void ex1___28();
+union point
+{
+    int x;
+    int y;
+};
+
+void ex1___27();
+union point
+{
+    int x;
+    int y;
+};
+struct student 
+{
+    int a;
+    int b;
+}
+
+
+void ex1___26();
+struct point
+{
+    int x;
+    int y;
+};
+struct point* function(void);
+struct point* function(void)
+
+
+
+void ex1___25();
+struct point
+{
+    int x;
+    int y;
+};
+
+struct point function(void);
+struct point function(void)
+
+void ex1___24();
+struct point
+{
+    int x;
+    int y;
+};
+
+void function(struct point call);
+
+
+
+void ex1___23();
+struct point
+{
+    int x;
+    int y;
+};
+
+void function(struct point call);
+
+
+void ex1___22();
+struct point
+{
+    int x;
+    int y;
+};
+struct student
+{
+    char name[20];
+    struct point* link;
+};
+
+
+
+void ex1___21();
+struct student
+{
+    char name[20];
+    int money;
+    struct student* left_link;
+    struct student* right_link;
+};
+
+
+void ex1___20();
+struct student
+{
+    char name[20];
+    int money;
+    struct student* link;
+};
+
+void ex1___19();
+struct student
+{
+    char no[10];
+    char name[20];
+    double total;
+};
+
+
+void ex1___18();
+struct student stu
+{
+    char no[10];
+    char name[20];
+    double total;
+};
+
+void ex1___17();
+void ex1___16();
+void ex1___15();
+void ex1___14();
+void ex1___13();
+void ex1___12();
+void ex1___11();
+void ex1___10();
+void ex1___9();
+void ex1___8();
+
+void ex1___7();
+struct score
+{
+    double math;
+    double english;
+    double total;
+};
+struct student
+{
+     int no;
+     struct score s;
+};
+
+void ex1___6();
+void ex1___5();
+
+void ex1___4();
+struct 
+{
+    int x;
+    int y;
+};
+
+void ex1___3();
+void ex1___2();
+void ex1___1();
+
+
+/////////////////////// part 3
+
+
+void ex5__16();
+void ex5__15();
+void ex5__14();
+void ex5__13();
+void ex5__12();
+void ex5__11();
+void ex5__10();
+void ex5__9();
+void ex5__8();
+void ex5__7();
+void ex5__6();
+void ex5__5();
+void ex5__4();
+void ex5__3();
+void ex5__2();
+void ex5__1();
+
+void ex4__32();
+void ex4__31();
+void ex4__29();
+void ex4__28();
+void ex4__27();
+void ex4__26();
+void ex4__25();
+void ex4__24();
+void ex4__23();
+void ex4__22();
+void ex4__21();
+void ex4__20();
+void ex4__19();
+void ex4__18();
+void ex4__17();
+void ex4__16();
+void ex4__15();
+void ex4__14();
+void ex4__13();
+void ex4__12();
+void ex4__11();
+void ex4__10();
+void ex4__9();
+void ex4__8();
+void ex4__7();
+void ex4__6();
+void ex4__5();
+void ex4__4();
+void ex4__3();
+void ex4__2();
+void ex4__1();
+
+
+void ex3__15();
+void ex3__14();
+void ex3__13();
+void ex3__12();
+void ex3__11();
+void ex3__10();
+void ex3__9();
+void ex3__8();
+void ex3__7();
+void ex3__6();
+void ex3__5();
+void ex3__4();
+void ex3__3();
+void ex3__2();
+void ex3__1();
+
+void ex2__12();
+void ex2__11();
+void ex2__10();
+void ex2__9();
+void ex2__8();
+void ex2__7();
+void ex2__6();
+void ex2__5();
+void ex2__4();
+void ex2__3();
+void ex2__2();
+void ex2__1();
+
+
+void ex1__9();
+void ex1__8();
+void ex1__7();
+void ex1__6();
+void ex1__5();
+void ex1__4();
+void ex1__3();
+void ex1__2();
+void ex1__1();
+
+//////////////part 2 
+
+void ex9_14();
+void ex9_13();
+void ex9_12();
+void ex9_11();
+void ex9_10();
+void ex9_9();
+void ex9_8();
+void ex9_7();
+void ex9_6();
+void ex9_5();
+void ex9_4();
+void ex9_3();
+void ex9_2();
+
+void ex8_13();
+void ex8_12();
+void ex8_11();
+void ex8_10();
+void ex8_9();
+void ex8_8();
+void ex8_7();
+void ex8_6();
+void ex8_5();
+void ex8_4();
+void ex8_3();
+void ex8_2();
+void ex8_1();
+
+void ex7_16();
+void ex7_15();
+void ex7_14();
+void ex7_13();
+void ex7_12();
+void ex7_11();
+void ex7_10();
+void ex7_9();
+void ex7_8();
+void ex7_7();
+void ex7_6();
+void ex7_5();
+void ex7_4();
+void ex7_3();
+void ex7_2();
+void ex7_1();
+
+void ex6_14();
+void ex6_13();
+void ex6_12();
+void ex6_11();
+void ex6_10();
+void ex6_9();
+void ex6_8();
+void ex6_7();
+void ex6_6();
+void ex6_5();
+void ex6_4();
+void ex6_3();
+void ex6_2();
+void ex6_1();
+
+void ex5_18();
+void ex5_17();
+void ex5_16();
 void ex5_15();
 void ex5_14();
 void ex5_13();
